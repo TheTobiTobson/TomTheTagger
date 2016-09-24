1c3ac2d47b9b848a605a5ff109c17bf14d23dac0
TheTobiTobson <tobi.wurm@gmail.com>
2016-09-24 12:37:59 +0200

    DatabaseManager eingefügt
    DatabaseManager eingefügt
    > Dummy Suchfunktion implementiert

--- TomTheTagger/MainWindow.xaml.cs
+++ TomTheTagger/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace TomTheTagger
 {
@@ -20,9 +22,56 @@ namespace TomTheTagger
     /// </summary>
     public partial class MainWindow : Window
     {
+        DatabaseManager oDataBase = new DatabaseManager("JSONTestData.txt");
+
         public MainWindow()
         {
             InitializeComponent();
+            oDataBase.LoadJsonDatabaseFile();        
+        }
+
+        private void TagBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                MessageBox.Show(oDataBase.SearchDatabaseForTag("hi").ToString());
+            }
+        }
+    }
+
+    public class TaggedFile
+    {
+        public string Path { get; set; }
+        public bool Active { get; set; }
+        public IList<string> Tags { get; set; }
+    }
+
+    class DatabaseManager
+    {
+        public string mDatabaseLocation; //Location of JSON Database file
+        public List<TaggedFile> mTaggedFileListe; //Database in RAM
+
+        // Constructor
+        public DatabaseManager(string pDatabaseLocation)
+        {
+            mDatabaseLocation = pDatabaseLocation;
+        }
+        
+        // Load Database from File to RAM
+        public void LoadJsonDatabaseFile()
+        {
+            string JsonDatabaseFileInString = File.ReadAllText(mDatabaseLocation);
+            mTaggedFileListe = JsonConvert.DeserializeObject<List<TaggedFile>>(JsonDatabaseFileInString);
+        }
+
+        public List<TaggedFile> SearchDatabaseForTag(string pTagToSearchFor)
+        {
+            List<TaggedFile> localSearchResults = new List<TaggedFile>();
+
+            localSearchResults.Add(mTaggedFileListe[0]);
+            localSearchResults.Add(mTaggedFileListe[2]);
+
+            return localSearchResults;
         }
     }
 }
