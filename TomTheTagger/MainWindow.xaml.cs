6e20b22ad76e346d7166d684ead7dc17e84a7e13
TheTobiTobson <tobi.wurm@gmail.com>
2016-10-14 20:50:05 +0200

    Aufbau einer neuen Klasse SearchOperations
    Klasse kann bereits nach mehreren Tags suchen und die Ergebnisse in eine
    Liste mit IdentNr und mehreren Tags mergen

--- TomTheTagger/MainWindow.xaml.cs
+++ TomTheTagger/MainWindow.xaml.cs
@@ -56,6 +56,34 @@ namespace TomTheTagger
         public string mOneTagInThisFileThatCorrespondsToQuery { get; set; }
     }
 
+    // Pair of one IdentNr and one Tag
+    class identTagPair
+    {
+        public int mIdentNr { get; set; }
+        public string mOneTag { get; set; }
+    }
+
+    // One IdentNr and multiple Tags
+    class identAndMultipleTags
+    {
+        public int mIdentNr { get; set; }
+        public List<string> mAllTagInThisFileThatCorrespondsToQuery { get; set; }
+
+        // Constructor
+        public identAndMultipleTags(int pIdentNr, string pTagsToAdd)
+        {
+            mAllTagInThisFileThatCorrespondsToQuery = new List<string>();
+
+            mIdentNr = pIdentNr;
+            mAllTagInThisFileThatCorrespondsToQuery.Add(pTagsToAdd);
+        }
+
+        public void addTag(string pNewTag)
+        {
+            mAllTagInThisFileThatCorrespondsToQuery.Add(pNewTag);
+        }
+    }
+
     // Data for GUI
     class SearchResultsForGUI
     {
@@ -73,6 +101,7 @@ namespace TomTheTagger
     public partial class MainWindow : Window
     {
         DatabaseManager oDataBase = new DatabaseManager("JSONTestData.txt");
+        SearchOperations oSearchOps = new SearchOperations();
 
         // List with search Results
         List<SearchRelatedInfosMerged> oSearchResultsWithCorrespondingTags = new List<SearchRelatedInfosMerged>();
@@ -88,13 +117,117 @@ namespace TomTheTagger
         {
             if(e.Key == Key.Enter)
             {
-                oSearchResultsWithCorrespondingTags = oDataBase.SearchDatabaseForTag(TagBox1.Text, TagBox2.Text, TagBox3.Text, TagBox4.Text);
-                lvUsers.ItemsSource = oDataBase.JoinListsForGui(oSearchResultsWithCorrespondingTags);
+                string[] tagsFromGui = { TagBox1.Text, TagBox2.Text, TagBox3.Text, TagBox4.Text };
+                oSearchOps.searchForMultipleTags(tagsFromGui, oDataBase.mTaggedFileListe);
+                //oSearchResultsWithCorrespondingTags = oDataBase.SearchDatabaseForTag(TagBox1.Text, TagBox2.Text, TagBox3.Text, TagBox4.Text);
+                //lvUsers.ItemsSource = oDataBase.JoinListsForGui(oSearchResultsWithCorrespondingTags);
             }
         }
     }
 
-    
+    public class SearchOperations
+    {
+        internal List<SearchRelatedInfosMerged> searchForMultipleTags(string[] pSearchtagsFromGui, List<TaggedFile> pjsonDatabaseInRam)
+        {   
+            List<identTagPair>[] arrayOfIdentTagPairLists = new List<identTagPair>[10]; // später pSearchtagsFromGui.Length()
+            int amountOfTagsToBeSearchedFor = 0;
+
+            // Check if Tags are valid and count
+            foreach (var item in pSearchtagsFromGui){
+                if (item.Length != 0){
+                    amountOfTagsToBeSearchedFor++;
+                }
+            }
+
+            //if(amountOfTagsToBeSearchedFor == 0) // Schlau abbrechen
+
+            // Excecute a search for every tag
+            for (int i = 0; i < amountOfTagsToBeSearchedFor; i++)
+            {
+                arrayOfIdentTagPairLists[i] = searchForOneTag(pSearchtagsFromGui[i], pjsonDatabaseInRam);
+                //if( arrayOfIdentTagPairLists[0].Count == 0) // Kein Tag gefunden > Daten nicht und nicht mergen
+            }
+
+            // initialize List<identAndMultipleTags> with identTagPair for the first Tag
+            List<identAndMultipleTags> identAndMultipleTagsList = initIdentAndMultipleTagsList(arrayOfIdentTagPairLists[0]);
+
+            // merge all identTagPairs 
+            mergeOneIdentTagPair(arrayOfIdentTagPairLists[1], identAndMultipleTagsList);
+            mergeOneIdentTagPair(arrayOfIdentTagPairLists[2], identAndMultipleTagsList);
+
+            List<SearchRelatedInfosMerged> TEST = new List<SearchRelatedInfosMerged>();
+            return TEST;
+
+        }
+
+        /// <summary>
+        /// Method to search for one Tag in Database
+        /// </summary>
+        /// <param name="pTagToBeSearcheFor"></param>
+        /// <param name="pjsonDatabaseInRam"></param>
+        /// <returns></returns>
+        private List<identTagPair> searchForOneTag(string pTagToBeSearcheFor, List<TaggedFile> pjsonDatabaseInRam)
+        {
+            List<identTagPair> localIdentTagPairList = new List<identTagPair>();
+
+           var Search = from TaggedFiles in pjsonDatabaseInRam
+                         from TagsinTaggedFiles in TaggedFiles.Tags
+                         where TagsinTaggedFiles == pTagToBeSearcheFor
+                         select new identTagPair() { mIdentNr = TaggedFiles.IdentNr, mOneTag = pTagToBeSearcheFor };
+
+            localIdentTagPairList = Search.ToList();
+
+            return localIdentTagPairList;
+        }
+
+        /// <summary>
+        /// Method to initialize List<identAndMultipleTags> which will be use to gather all search results
+        /// </summary>
+        /// <param name="pIdentTagPairListForInit"></param>
+        /// <returns></returns>
+        private List<identAndMultipleTags> initIdentAndMultipleTagsList(List<identTagPair> pIdentTagPairListForInit)
+        {
+            List<identAndMultipleTags> oLocalIdentAndMultipleTagsList = new List<identAndMultipleTags>();
+
+            foreach (var item in pIdentTagPairListForInit)
+            {
+                oLocalIdentAndMultipleTagsList.Add(new identAndMultipleTags(item.mIdentNr, item.mOneTag));
+            }
+
+            return oLocalIdentAndMultipleTagsList;
+        }
+
+        /// <summary>
+        /// Method to merge identTagPair to identAndMultipleTagsList
+        /// </summary>
+        /// <param name="pIdentTagPairToMerge"></param>
+        /// <param name="pidentAndMultipleTagsList"></param>
+        private void mergeOneIdentTagPair(List<identTagPair> pIdentTagPairToMerge, List<identAndMultipleTags> pidentAndMultipleTagsList)
+        {
+            foreach (var IdentTagPairItem in pIdentTagPairToMerge)
+            {
+                bool subLoopEnteredSomethingInMergedInfo = false;
+
+                foreach (var IdentAndMultipleTagItem in pidentAndMultipleTagsList)
+                {
+                    if (IdentTagPairItem.mIdentNr == IdentAndMultipleTagItem.mIdentNr)
+                    {
+                        
+                        IdentAndMultipleTagItem.addTag(IdentTagPairItem.mOneTag);
+                        subLoopEnteredSomethingInMergedInfo = true;
+
+                        // There is only one matching IdentNr in IdentMULTIPLEtagCombi >> work is done for this loop
+                        break;
+                    }
+                }
+
+                if (!subLoopEnteredSomethingInMergedInfo)
+                {
+                    pidentAndMultipleTagsList.Add(new identAndMultipleTags(IdentTagPairItem.mIdentNr, IdentTagPairItem.mOneTag));
+                }
+            }
+        }
+    }
 
     class DatabaseManager
     {
